70fbfcffe1a5a77066172a5452b52d9080518b67
WANDERLEY CABRAL DA  SILVA <wcabraldasilvac@gmail.com>
2022-05-03 15:14:03 -0300

    Update calculadora.java
    Adicionando comentários no projeto

--- calculadora.java
+++ calculadora.java
@@ -1,6 +1,7 @@
 
 // Meu primeiro programa em JAVA
-// Simples calculadora em JAVA
+// Uma Simples calculadora em JAVA
+
 import java.util.Scanner;
 
 public class calculadora {
@@ -8,17 +9,17 @@ public class calculadora {
   public static void main(String[] args) {
     try (Scanner scan = new Scanner(System.in)) {
       int a, b;
-
+// Entrada de dados
       System.out.println("Digite o primeiro valor: ");
       a = scan.nextInt();
       System.out.println("Digite o segundo valor: ");
       b = scan.nextInt();
-
+// Armazena dados
       int soma = soma(a, b);
       int subtracao = subtracao(a, b);
       int multiplicacao = multiplicacao(a, b);
       int divisao = divisao(a, b);
-
+// Mostra dados
       System.out.println("Resultado da soma: " + soma);
       System.out.println("Resultado da sub.: " + subtracao);
       System.out.println("Resultado da mult.: " + multiplicacao);
